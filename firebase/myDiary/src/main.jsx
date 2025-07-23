2e6aa1f778552e6e5c9fec12d3de5493a3c6a65e
nugurejeil <nugurejeil1@gmail.com>
2025-07-23 12:16:26 +0900

    0723 중간 업로드(firebase)

---
+++ firebase/myDiary/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'normalize.css'
+import './main.css'
+import App from './App.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+)
